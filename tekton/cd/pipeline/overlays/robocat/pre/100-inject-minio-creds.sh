f91c39840256c5e4db99ec72cc436f00ca7283c7
Andrea Frittoli <andrea.frittoli@gmail.com>
2020-03-18 17:07:44 +0000

    Grant tekton-deployer access to minio secret
    And to a number of other resources required to deploy Tekton.

--- tekton/cd/pipeline/overlays/robocat/pre/100-inject-minio-creds.sh
+++ tekton/cd/pipeline/overlays/robocat/pre/100-inject-minio-creds.sh
@@ -2,6 +2,9 @@
 #
 # Grab Minio credentials and injects them in the storage secret used by
 # Tekton Pipeline
+set -exo pipefail
+
+echo "=== Start Pre-script Inject Minio Creds"
 BASE_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 MINIO_NAMESPACE=${MINIO_NAMESPACE:-eu-geo}
@@ -12,6 +15,8 @@ MINIO_ACCESS_KEY=$(kubectl get -n $MINIO_NAMESPACE secret/$MINIO_SECRET \
 MINIO_SECRET_KEY=$(kubectl get -n $MINIO_NAMESPACE secret/$MINIO_SECRET \
   -o jsonpath='{ .data.secretkey }' | base64 -d)
 
-sed -e 's/__MINIO_ACCESS_KEY__/'$MINIO_ACCESS_KEY'/g' \
-  -e 's/__MINIO_SECRET_KEY__/'$MINIO_SECRET_KEY'/g' \
+sed -e 's,__MINIO_ACCESS_KEY__,'$MINIO_ACCESS_KEY',g' \
+  -e 's,__MINIO_SECRET_KEY__/,'$MINIO_SECRET_KEY',g' \
   "${BASE_DIR}/tekton-storage-secret.yaml.tpl" > "${BASE_DIR}/../tekton-storage-secret.yaml"
+
+echo "=== End Pre-script Inject Minio Creds"
