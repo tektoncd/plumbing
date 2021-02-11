990bd169f302d195ac9176177d583669b65cddcd
vinamra28 <vinjain@redhat.com>
2021-02-11 19:25:21 +0530

    Add Dir support in Catlin
    - With the patch now you can run catlin on directories and also you can
    now run catlin on multiple inputs.
    
    Sample execution command would be
    
    ```sh
    catlin validate task/black/* task/maven/*
    ```
    
    Signed-off-by: vinamra28 <vinjain@redhat.com>

--- catlin/pkg/cmd/validate/validate.go
+++ catlin/pkg/cmd/validate/validate.go
@@ -17,6 +17,7 @@ package validate
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -26,12 +27,40 @@ import (
 	"github.com/tektoncd/plumbing/catlin/pkg/validator"
 )
 
+func getFilesInDir(path string) ([]os.FileInfo, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
 func fileExists(path string) bool {
 	info, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
 	}
-	return !info.IsDir()
+
+	if info.IsDir() {
+
+		files, err := getFilesInDir(path)
+		if err != nil {
+			return false
+		}
+
+		for _, file := range files {
+			if filepath.Ext(file.Name()) == ".yaml" {
+				return true
+			}
+		}
+		return false
+	}
+
+	return true
 }
 
 func validResourcePath() cobra.PositionalArgs {
@@ -40,9 +69,12 @@ func validResourcePath() cobra.PositionalArgs {
 			return fmt.Errorf("requires at least 1 path to tekton resource yaml but received none")
 		}
 
-		if !fileExists(args[0]) {
-			return fmt.Errorf("no such file - %s", args[0])
+		for _, filePath := range args {
+			if !fileExists(filePath) {
+				return fmt.Errorf("valid Tekton resource not found in the path - %s", filePath)
+			}
 		}
+
 		return nil
 	}
 }
@@ -53,9 +85,47 @@ func Command(cli app.CLI) *cobra.Command {
 		Aliases: []string{"verify"},
 		Args:    validResourcePath(),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return validate(cli, args[0])
+			return validateResources(cli, args)
 		},
 	}
+}
+
+func validateResources(cli app.CLI, args []string) error {
+
+	out := cli.Stream().Out
+	for _, filePath := range args {
+		info, _ := os.Stat(filePath)
+		if info.IsDir() {
+
+			files, err := getFilesInDir(filePath)
+			if err != nil {
+				return fmt.Errorf(err.Error())
+			}
+
+			for _, file := range files {
+				if filepath.Ext(file.Name()) == ".yaml" {
+					fileWithPath := filePath
+					if strings.HasSuffix(fileWithPath, "/") {
+						fileWithPath = fileWithPath + file.Name()
+					} else {
+						fileWithPath = fileWithPath + "/" + file.Name()
+					}
+					fmt.Fprintf(out, "FILE: %s\n", fileWithPath)
+					err = validate(cli, fileWithPath)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		} else if filepath.Ext(filePath) == ".yaml" {
+			fmt.Fprintf(out, "FILE: %s\n", filePath)
+			err := validate(cli, filePath)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func validate(cli app.CLI, path string) error {
