ccb969f16279c607ab89d785f0e600bf57505ba0
vinamra28 <vinjain@redhat.com>
2021-02-16 12:59:54 +0530

    Add tests for catlin validate command
    As of now no unit tests were present for `catlin validate` subcommand.
    
    The following commit contains:-
     - testdata in which 2 tasks are present with their proper directory
       structure.
     - cobra.go file which contains helper function ExecuteCommandC which
       will help us to execute command.
     - validate_test.go which contains the test cases for validate
       subcommand.
    
    Signed-off-by: vinamra28 <vinjain@redhat.com>

---
+++ catlin/pkg/cmd/validate/validate_test.go
@@ -0,0 +1,59 @@
+// Copyright © 2021 The Tekton Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package validate
+
+import (
+	"testing"
+
+	"github.com/tektoncd/plumbing/catlin/pkg/app"
+	"github.com/tektoncd/plumbing/catlin/pkg/test"
+	"gotest.tools/v3/assert"
+)
+
+func TestValidate(t *testing.T) {
+
+	testParams := []struct {
+		name      string
+		args      []string
+		wantError bool
+		want      string
+	}{
+		{
+			name:      "single filepath",
+			args:      []string{"./testdata/task/maven/0.1"},
+			wantError: false,
+			want:      "",
+		},
+		{
+			name:      "multiple filepath",
+			args:      []string{"./testdata/task/maven/0.1", "./testdata/task/npm/0.1"},
+			wantError: false,
+			want:      "",
+		},
+	}
+
+	for _, tp := range testParams {
+		t.Run(tp.name, func(t *testing.T) {
+			cli := app.New()
+			validate := Command(cli)
+			got, err := test.ExecuteCommand(validate, tp.args...)
+			if !tp.wantError {
+				if err != nil {
+					t.Errorf("Unexpected error")
+				}
+				assert.Equal(t, tp.want, got)
+			}
+		})
+	}
+}
