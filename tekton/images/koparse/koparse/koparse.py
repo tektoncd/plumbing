191d4e346c0ebb212de6cd489211cd4ae488c2a2
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2024-09-18 10:45:57 +0200

    Add koparse support for no-preserve-path
    Add support for the case of path not preserved in ko.
    The implementation is done in a backwards compatible
    way so that if ko is invoked with the same parameters
    as before it will work in the same way.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- tekton/images/koparse/koparse/koparse.py
+++ tekton/images/koparse/koparse/koparse.py
@@ -16,7 +16,7 @@ This script does two things:
 """
 
 import argparse
-import os
+import hashlib
 import re
 import string
 import sys
@@ -69,7 +69,24 @@ def parse_release(base: str, path: str) -> List[str]:
     return images
 
 
-def compare_expected_images(expected: List[str], actual: List[str]) -> None:
+def md5(input: str) -> str:
+    return hashlib.md5(input.encode()).hexdigest()
+
+
+def convert_image_path(image: str) -> str:
+    # Separate the image name from path in <container-reg>/<path>/<image-name>:<tag>
+    parts = image.split(":")
+    image_without_tag = parts[0]
+    tag = ""
+
+    if len(parts) == 2:
+        tag = f":{parts[1]}"
+    
+    image_name = image_without_tag.split("/")[-1]
+
+    return f"{image_name}-{md5(image_without_tag)}{tag}"
+
+def compare_expected_images(expected: List[str], actual: List[str], container_registry: str, preserve_path: bool) -> None:
     """Ensures that the list of actual images includes only the expected images
 
     Args:
@@ -82,6 +99,10 @@ def compare_expected_images(expected: List[str], actual: List[str]) -> None:
         if DIGEST_MARKER not in image:
             raise BadActualImageFormatError(image)
 
+    if not preserve_path:
+        expected = [convert_image_path(image) for image in expected]
+
+    expected = ["/".join([container_registry, image]) for image in expected]
     actual_no_digest = [image.split(DIGEST_MARKER)[0] for image in actual]
 
     missing = set(expected) - set(actual_no_digest)
@@ -91,20 +112,51 @@ def compare_expected_images(expected: List[str], actual: List[str]) -> None:
         raise ImagesMismatchError(list(missing), list(extra))
 
 
+def backwards_compatible_params(container_registry: str, base: str, images: List[str]) -> tuple[str, str, List[str]]:
+    # For backwards compatibility, if container registry is not provided, we assume it's
+    # the first section of the base, which is the legacy behavior. We then need to strip
+    # container registry out of the expected images and base
+    if not container_registry:
+        container_registry = "/".join(base.split("/")[:2])
+        base = "/".join(base.split("/")[2:])
+        images = ["/".join(image.split("/")[2:]) for image in images]
+    return container_registry, base, images
+
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(
         description="Parse expected built images from a release.yaml created by `ko`")
     arg_parser.add_argument("--path", type=str, required=True,
                             help="Path to the release.yaml")
+    arg_parser.add_argument("--container-registry", dest="container_registry", type=str, required=False,
+                            help="Container registry URI and path e.g. gcr.io/tekton-releases")
     arg_parser.add_argument("--base", type=str, required=True,
                             help="String prefix which is used to find images within the release.yaml")
     arg_parser.add_argument("--images", type=str, required=True, nargs="+",
                             help="List of all images expected to be built, without digests")
+    arg_parser.add_argument("--preserve-path", dest="preserve_path", type=bool, required=False, default=True,
+                            help="Whether ko is configured to preserve the images base path")
     args = arg_parser.parse_args()
 
     try:
-        images = parse_release(args.base, args.path)
-        compare_expected_images(args.images, images)
+        container_registry, expected_images, base = backwards_compatible_params(
+            args.container_registry, args.images, args.base)
+
+        # For backwards compatibility, if container registry is not provided, we assume it's
+        # the first section of the base, which is the legacy behavior. We then need to strip
+        # container registry out of the expected images and base
+        if not args.container_registry:
+            container_registry = "/".join(args.base.split("/")[:1])
+            base = "/".join(args.base.split("/")[2:])
+            expected_images = ["/".join(image.split("/")[2:]) for image in args.images]
+
+        if args.preserve_path:
+            search_path = "/".join([container_registry, args.base])
+        else:
+            search_path = container_registry
+
+        images = parse_release(search_path, args.path)
+        compare_expected_images(images, images, container_registry, args.preserve_path)
     except (IOError, BadActualImageFormatError) as e:
         sys.stderr.write("Error determining built images: %s\n" % e)
         sys.exit(1)
